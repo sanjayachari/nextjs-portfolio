ea4c9bb71a56702de8c423cf984d6ef3ba80b1bb
Sanjay <sanjayacharya992@.com>
2024-07-11 01:14:42 +0530

    new skin of portfolio and update

---
+++ app/components/ProductOverview.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React from "react";
+import { VscDebugBreakpointLog } from "react-icons/vsc";
+
+const ProductOverview = () => {
+  return (
+    <div className="p-4 h-full ">
+      <div className="text-3xl md:text-6xl font-bold text-center opacity-70 md:flex items-center justify-center">
+        Product Overview <span className="md:block hidden mx-2">|</span> <Link href="https://account.boostopia.io" className="underline text-blue-500 text-[30px] " target="_blank">Link</Link>
+      </div>
+      <ul className="p-1 md:p-10">
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Platform Description: Describe Boostopia as
+          a platform designed for creators and influencers to monetize their
+          influence through product sales and mentoring.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Emergency Call via WhatsApp: Enables users
+          to make emergency calls using WhatsApp for quick response.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Creating Services: Users can create various
+          services within SuperDM, including features like 1:1 video calls.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Sharing via LinkedIn: Facilitates sharing of
+          services on LinkedIn for broader visibility and engagement.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Centralized Link Management: Provides a
+          unified platform to gather and manage all service-related links
+          efficiently.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Customizing Profile Pages: Allows
+          customization of user profiles to showcase expertise and enhance
+          personal branding.
+        </li>
+        <li className="flex gap-2 items-center my-5 border border-black border-opacity-30 p-2 rounded-md text-[16px]">
+          <VscDebugBreakpointLog /> Collaboration Features: Supports seamless
+          collaboration among users for joint projects and initiatives.
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProductOverview;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 import Header from './components/Header.jsx'
+import About from './components/About.jsx'
+import Project from './components/Project.jsx'
+import Resume from './components/Resume.jsx'
+import Experience from './components/Experience';
+import "./components/header.css";
+import ProductOverview from './components/ProductOverview';
+
 export default function Home() {
   return (
-    <main className="h-full w-full">
+    <main className="fo h-full w-full">
 
     <Header/>
+    <div className="bg-[#0C0C0C] md:p-5 p-0">
+
+    <About />
+    </ div>
+    <Experience/>
+    <ProductOverview/>
+    <Project/>
+    <Resume/>
+
     </main>
   );
 }
